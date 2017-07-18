7c1514ccf911ce99ce9fc41275386ff001b43687
Someone <Someone@Someone-PC.mshome.net>
2017-07-18 13:36:55 +0700

    Revert "show multiple mark at map"
    This reverts commit ab112c7b35ddf321e29a185d982112e90032ee96.

--- TNIAL/src/Controller/ControlMaps.java
+++ TNIAL/src/Controller/ControlMaps.java
@@ -1,6 +1,5 @@
 package Controller;
 
-import Model.TunDinas;
 import org.openstreetmap.gui.jmapviewer.JMapViewer;
 import org.openstreetmap.gui.jmapviewer.JMapViewerTree;
 import org.openstreetmap.gui.jmapviewer.MapMarkerDot;
@@ -15,33 +14,16 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.List;
 import java.awt.Point;
-import java.awt.Stroke;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.event.MouseInputListener;
-import org.eclipse.persistence.internal.databaseaccess.DatabaseAccessor;
 import org.openstreetmap.gui.jmapviewer.Coordinate;
-import org.openstreetmap.gui.jmapviewer.Layer;
-import org.openstreetmap.gui.jmapviewer.LayerGroup;
-import org.openstreetmap.gui.jmapviewer.MapMarkerCircle;
-import org.openstreetmap.gui.jmapviewer.MapRectangleImpl;
-import org.openstreetmap.gui.jmapviewer.Style;
 
 
 /**
@@ -59,8 +41,6 @@ public class ControlMaps extends JPanel implements JMapViewerEventListener {
     private JLabel mperpLabelName;
     private JLabel mperpLabelValue;
     private boolean clicked;
-    private ArrayList<TunDinas> dataTunDinas = new ArrayList<>();
-    private Database.DB4SQLServer db = new Database.DB4SQLServer();
 
 
     /**
@@ -82,40 +62,57 @@ public class ControlMaps extends JPanel implements JMapViewerEventListener {
         map().setTileLoader(new OsmTileLoader(map()));
         map().setMapMarkerVisible(true);
         map().setZoomContolsVisible(true);
-        
-        
-        addMarkTunDinas();
-        
+
+        // activate map in window
+        //treeMap.setTreeVisible(true);
         add(treeMap, BorderLayout.CENTER);
         
-    }
-    
-    public ControlMaps(String x, String y){
-        treeMap = new JMapViewerTree("Zones");
-        setupJFrame();
-        map().addJMVListener(this);
-        map().setTileSource(new OsmTileSource.Mapnik());
-        map().setTileLoader(new OsmTileLoader(map()));
-        map().setMapMarkerVisible(true);
-        map().setZoomContolsVisible(true);
-                
-        Style legend1 = new Style(Color.BLACK, Color.GREEN, null, null);
-        MapMarkerDot dadot = new MapMarkerDot(new Coordinate(Double.valueOf(y), Double.valueOf(x)));
-        dadot.setStyle(legend1);
-        map().addMapMarker(dadot);
-                                
-        add(treeMap, BorderLayout.CENTER);
     }
     
     private void setupJFrame() {
-        setSize(600, 600);
+        setSize(400, 400);
         setLayout(new BorderLayout());
     }    
+    
+//    public void setupPanels() {
+//        // not yet called this method
+//        JPanel panel = new JPanel();
+//        JPanel panelTop = new JPanel();
+//        JPanel panelBottom = new JPanel();
+//        JPanel helpPanel = new JPanel();
+//
+//        mperpLabelName = new JLabel("Meters/Pixels: ");
+//        mperpLabelValue = new JLabel(String.format("%s", map().getMeterPerPixel()));
+//        zoomLabel = new JLabel("Zoom: ");
+//        zoomValue = new JLabel(String.format("%s", map().getZoom()));
+//
+//        add(panel, BorderLayout.NORTH);
+//        add(helpPanel, BorderLayout.SOUTH);
+//        panel.add(panelTop, BorderLayout.NORTH);
+//        panel.add(panelBottom, BorderLayout.SOUTH);
+//        JLabel helpLabel = new JLabel("Use right mouse button to move,\n "
+//                + "left double click or mouse wheel to zoom.");
+//        helpPanel.add(helpLabel);
+//
+//        panelTop.add(zoomLabel);
+//        panelTop.add(zoomValue);
+//        panelTop.add(mperpLabelName);
+//        panelTop.add(mperpLabelValue);
+//    }
+
 
     public JMapViewer map() {
         return treeMap.getViewer();
     }
     
+//    public void coordinate(){
+//        if(clicked=true){
+//            Coordinate coord = null;
+//            map().getMapPosition(coord);
+//            System.out.println(coord);
+//        }
+//    }
+
     public void updateZoomParameters() {
         if (mperpLabelValue != null)
             mperpLabelValue.setText(String.format("%s", map().getMeterPerPixel()));
@@ -129,51 +126,6 @@ public class ControlMaps extends JPanel implements JMapViewerEventListener {
         if (command.getCommand().equals(JMVCommandEvent.COMMAND.ZOOM) ||
                 command.getCommand().equals(JMVCommandEvent.COMMAND.MOVE)) {
             updateZoomParameters();
-        }
-    }
-    
-    public void addMarkTunDinas(){
-        Style legend1 = new Style(Color.BLACK, Color.GREEN, null, null);
-        getDataDBTunDinas();
-        ArrayList<MapMarkerDot> dotList = new ArrayList<>();
-        
-        for(int i=0; i< dataTunDinas.size(); i++){
-            dotList.add(new MapMarkerDot(dataTunDinas.get(i).getDasar(), dataTunDinas.get(i).getCoor()));
-            dotList.get(i).setStyle(legend1);
-            map().addMapMarker(dotList.get(i));
-        }        
-    }
-    
-    public void getDataDBTunDinas(){
-        try {
-            //Class.forName(driver);
-            Connection con = DriverManager.getConnection(this.db.getURL());
-            PreparedStatement st = con.prepareStatement("SELECT * FROM bankum_tundinas");
-            ResultSet rs = st.executeQuery();
-            //rs.beforeFirst();
-            int n = 0;            
-            while(rs.next()){
-                this.dataTunDinas.add(new TunDinas(
-                        rs.getString("idTundinas"),
-                        rs.getString("lokasiDT"),
-                        rs.getString("Dasar"),
-                        rs.getString("noSurat"),
-                        rs.getDate("tglDasar"),
-                        rs.getString("Permasalahan")
-                ));
-                String[] coor = new String[2];
-                if(rs.getString("Koordinat")!=null){
-                    coor = rs.getString("Koordinat").split(",");
-                }else{
-                    coor[0] = "0";
-                    coor[1] = "0";
-                }   
-                this.dataTunDinas.get(n).setCoor(Double.valueOf(coor[0]), Double.valueOf(coor[1]));
-                n++;
-            }                
-            rs.close();
-        } catch (SQLException ex) {
-            Logger.getLogger(TunDinas.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
